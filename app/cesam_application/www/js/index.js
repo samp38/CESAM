002e64c84d805d9e5c662ca4c7ecea166e64e3c1
Sam <samuel.heidmann@gmail.com>
2024-06-07 17:03:57 +0200

    App : add int2bytes and bytes2int helper functions

--- app/cesam_application/www/js/index.js
+++ app/cesam_application/www/js/index.js
@@ -20,6 +20,44 @@ function stringToBytes(string)
     return array.buffer;
 }
 
+function bitLength(number) {
+  return Math.floor(Math.log2(number)) + 1;
+}
+
+function byteLength(number) {
+  return Math.ceil(bitLength(number) / 8);
+}
+
+function toBytes(number) {
+  if (!Number.isSafeInteger(number)) {
+    throw new Error("Number is out of range");
+  }
+
+  const size = number === 0 ? 0 : byteLength(number);
+  const bytes = new Uint8ClampedArray(size);
+  let x = number;
+  for (let i = (size - 1); i >= 0; i--) {
+    const rightByte = x & 0xff;
+    bytes[i] = rightByte;
+    x = Math.floor(x / 0x100);
+  }
+
+  return bytes.buffer;
+}
+
+function fromBytes(buffer) {
+  const bytes = new Uint8ClampedArray(buffer);
+  const size = bytes.byteLength;
+  let x = 0;
+  for (let i = 0; i < size; i++) {
+    const byte = bytes[i];
+    x *= 0x100;
+    x += byte;
+  }
+  return x;
+}
+
+
 var cesam =
 {
     serviceUUID: '19B10010-E8F2-537E-4F6C-D104768A1214',
