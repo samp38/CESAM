f3bd4b0d726c71d089f8049e54a3b849e28fd639
Samuel <samuel.heidmann@inria.fr>
2025-10-17 16:47:46 +0200

    remove servopin from bsp

--- firmware/include/bsp.h
+++ firmware/include/bsp.h
@@ -6,11 +6,9 @@
 #ifdef NANO_33_BLE
   #define MOTOR_PIN1 D2
   #define MOTOR_PIN2 D3
-  #define SERVO_PIN D6
 #elif FEATHER_SENSE
   #define MOTOR_PIN1 A0
   #define MOTOR_PIN2 A1
-  #define SERVO_PIN 13
 #else
   #error "Board not supported! Define NANO_33_BLE or FEATHER_SENSE"
 #endif
